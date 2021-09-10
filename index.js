4aacee280dd396eec9e89a532157c29afc024723
Chris Donaldson(Digital Labs) <chris@digital-labs.co.uk>
2021-09-10 14:49:11 +0100

    new endpoint

--- index.js
+++ index.js
@@ -7,21 +7,9 @@ const app = express();
 // Configuration
 const PORT = 3000;
 const HOST = "localhost";
-const API_SERVICE_URL = "https://admin-reviti-int-01.underwriteme.co.uk/api";
+const API_SERVICE_URL = "https://reviti-int-02.underwriteme.co.uk/api";
 // Logging
 app.use(morgan('dev'));
-// Info GET endpoint
-app.get('/info', (req, res, next) => {
-    res.send('This is a proxy service which proxies to Billing and Account APIs.');
- });
- // Authorization
-// app.use('', (req, res, next) => {
-//     if (req.headers.authorization) {
-//         next();
-//     } else {
-//         res.sendStatus(403);
-//     }
-//  });
 
  // Proxy endpoints
 app.use(createProxyMiddleware({
